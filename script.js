79f3182506506385121672c6f566e574664a07a3
ilya5015 <83602272+ilya5015@users.noreply.github.com>
2022-12-26 23:34:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var audio = new Audio("./media/sound.mp3");
+
+function setIndicatorTrue() {
+  document
+    .getElementById("body")
+    .classList.replace("body__false", "body__true");
+  document.getElementById("indicator").innerHTML =
+    "Уровень бумажных полотенец ниже критического";
+  document
+    .getElementById("indicator")
+    .classList.replace("indicator__false", "indicator__true");
+}
+
+function setIndicatorFalse() {
+  document
+    .getElementById("body")
+    .classList.replace("body__true", "body__false");
+  document.getElementById("indicator").innerHTML = "Всё в порядке";
+  document
+    .getElementById("indicator")
+    .classList.replace("indicator__true", "indicator__false");
+}
+
+async function fetchIndicatorStatus() {
+  const response = await fetch(
+    "https://blynk.cloud/external/api/get?token=_enxatq87TlTVGKSIdvSkwJpoXs4Gs4o&v0"
+  );
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const responseData = await response.json();
+  console.log(responseData);
+
+  if (responseData === 1) {
+    setIndicatorTrue();
+  } else if (responseData === 0) {
+    setIndicatorFalse();
+  }
+  //   await fetch(
+  //     "https://blynk.cloud/external/api/get?token=iyDUGy_1jqMhI0gHxBQC3jvcM3wPzO7V&v0"
+  //   )
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`Request failed with status ${response.status}`);
+  //       }
+  //       console.log(response);
+  //       return response;
+  //     })
+  //     .then((data) => {
+  //       console.log(data.json(), "coco");
+  //       if (data === 1) {
+  //         setIndicatorFalse();
+  //       } else {
+  //         setIndicatorTrue();
+  //       }
+  //     })
+  //     .catch((error) => console.log(error));
+}
+
+fetchIndicatorStatus();
+
+setInterval(() => {
+  console.log("fetching");
+  fetchIndicatorStatus();
+}, "5000");
